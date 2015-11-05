9f02efdd508d910af33da8e0e348ffa610bbdc4f
Steven Crawford <crawfordsm@gmail.com>
2015-11-05 14:35:39 +1100

    added logo

--- hello.py
+++ hello.py
@@ -8,11 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'I <3 the Universe '
+    return "<img src='static/ILTU_white.png'>"
 
-@app.route('/query/sia')
-def sia():
-    ra = request.args.get('RA')
-    dec = request.args.get('Dec')
-    size = request.args.get('Size')
-    return 'You asked for RA = {}, Dec = {}, Size = {}'.format(ra, dec, size)
+if __name__ == '__main__':
+    app.run()
+
+
