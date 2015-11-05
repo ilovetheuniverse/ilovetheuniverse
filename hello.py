9f2eb2ddf1339ec2b71ba4b1795a08228fea9722
Steven Crawford <crawfordsm@gmail.com>
2015-11-05 16:34:16 +1100

    added a dotastro7 page

--- hello.py
+++ hello.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def hello():
     return "<img src='static/ILTU_white.png'>"
 
+@app.route('/dotastro7')
+def hello():
+    return open(dotastro7.html)
+
 if __name__ == '__main__':
     app.run()
 
